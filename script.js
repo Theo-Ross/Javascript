5cc85fe665a0d70028cf77d0f6bddd1363e962a0
Theo-Ross <theoross99@icloud.com>
2022-11-04 11:38:08 +0000

    Positive number checker

---
+++ script.js
@@ -0,0 +1,14 @@
+// 1. Write a function that takes a number and returns true if it is a positive number and false if it is a negative number.
+
+function isNumberPositive(number){
+    if(number >0){
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+console.log(isNumberPositive(3))
+
+
